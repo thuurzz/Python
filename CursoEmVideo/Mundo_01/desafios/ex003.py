de18a9a37704e48ad1a090318ff4c35b36af4dbb
thuurzz <thuur.vss@gmail.com>
2020-03-17 17:22:38 -0300

    Lista de exercícios Curso Em Vídeo - Guanabara

---
+++ CursoEmVideo/Mundo_01/desafios/ex003.py
@@ -0,0 +1,6 @@
+n1 = float(input('Digite um valor:'))
+n2 = float(input('digite outro:'))
+
+soma = n1 + n2
+
+print('A soma entre {} e {} é: {}'.format(n1, n2, soma))
---
+++ CursoEmVideo/Mundo_01/desafios/ex005.py
@@ -0,0 +1,3 @@
+n = int(input('digite um número inteiro:'))
+
+print('o número digitado foi {}, seu antecessor é {} e o sucessor {}'.format(n, n-1, n+1))
