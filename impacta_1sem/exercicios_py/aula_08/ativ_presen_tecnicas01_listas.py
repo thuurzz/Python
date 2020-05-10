ce28057c7a437098dfa93c0d4b2fb784a1c94a73
Arthur Vinicius <thuur.vss@gmail.com>
2020-05-10 01:29:14 -0300

    Aula de Python - Listas

---
+++ impacta_1sem/exercicios_py/aula_08/ativ_presen_tecnicas01_listas.py
@@ -0,0 +1,34 @@
+#Arthur Vincius Santos Silva RA: 1903365
+
+lst = ['cereal', 'banana', 'maçã', 'melão', 'iogurte']
+
+# a)
+def imprime_lista(lista):
+    for i in range(len(lst)):
+        print(f'{i} - {lst[i]}')
+# b)
+'''
+print(len(lst))
+'''
+# c)
+'''
+print(lst[3])
+'''
+# d)
+'''
+a = input('')
+lst.append(a)
+'''
+# e)
+'''
+a = input('')
+lst.append(a)
+imprime_lista(lst)
+'''
+# f)
+'''
+lst.pop(2)
+imprime_lista(lst)
+'''
+# g)
+imprime_lista(lst)
