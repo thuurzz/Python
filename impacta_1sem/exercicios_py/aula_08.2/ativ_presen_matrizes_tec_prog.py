23b5e391778c0254b0d9c0e6ffa7a6056462c43c
Arthur Vinicius <thuur.vss@gmail.com>
2020-05-17 15:08:24 -0300

    Python/Técnicas de Programação - Matrizes

--- impacta_1sem/exercicios_py/aula_08.2/ativ_presen_matrizes_tec_prog.py
+++ impacta_1sem/exercicios_py/aula_08.2/ativ_presen_matrizes_tec_prog.py
@@ -1,5 +1,7 @@
 #Arthur Vinicius Santos Silva RA:1903365
 #professor, trabalho de fds, não terminei por completo, irei revisar!
+
+
 def cria_matriz(linhas, colunas, preenchimento):
     matriz = [] #inicia a matriz vazia
     for i in range(linhas): #repete até o num de linhas
@@ -7,30 +9,35 @@ def cria_matriz(linhas, colunas, preenchimento):
         matriz.append(linha) #adiciona linhas na matriz
     return matriz #retorna a matriz
 
+
 def preenche_matriz(matriz):
-    linha = len(A)
-    coluna = len(A[0])
-    for i in range(linha):
-        for j in range(coluna):
-            matriz[i][j] = input("Posição [%d][%d]: " % (i, j))
+    for i in range(m):
+        matriz[i][0] = input('Digite o nome do aluno:')
+        for j in range(1, n):
+            matriz[i][j] = float(input(f"Digite a nota {j} do Aluno:"))
     return matriz
 
+
 m = int(input('Informe a quantidade de Alunos: \n'))
-n = 6
+n = 3
 A = cria_matriz(m,n,0)
 preenche_matriz(A)
 
+print('\n')
+
 maior_media = -1
 linha_maior_med = -1
-for i in A:
-    print(f'Aluno {A[i][0]}')
-    for j in i:
-         print(f'nota {A[i][j + 1]}')
-    media = sum(A[i][1:n]) / (n - 1)
+
+for i in range(m):
+    print(f'Aluno: {A[i][0]}')
+    for j in range(1, n):
+        print(f'Nota {j}: {A[i][j]}')
+    soma = sum(A[i][1:n])
+    media = soma / (n - 1)
     print(f'Média: {media}')
     print()
     if media > maior_media:
         maior_media = media
         linha_maior_med = i
 
-print(f'O aluno, {A[linha_maior_med[0]]} tem a maior média {maior_media}.')
+print(f'O aluno, {A[linha_maior_med][0]} tem a maior média: {maior_media}')
