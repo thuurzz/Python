0a32c6b0f619c27af9ec4219d02c9542aa34f6e7
Arthur Vinicius <thuur.vss@gmail.com>
2020-05-20 16:29:34 -0300

    AC5 Logic Prog - Python

--- impacta_1sem/exercicios_py/aula_08.1/AC5_logic+.py
+++ impacta_1sem/exercicios_py/aula_08.1/AC5_logic+.py
@@ -1,14 +1,15 @@
 # Criar uma função que recebe um vetor de inteiros como parâmetro e retorna o índice do último elemento ímpar do vetor.
 
-def indice_ult_impar(lista):
-    impares = 0
-    for i in range(len(lista)):
-        if lista[i] % 2 != 0:
-            impares = i
-    return impares
+
+def indice_ult_impar(lista): #cria função
+    impares = 0 #inica var para receber indice
+    for i in range(len(lista)): #itera todos os itens da lista
+        if lista[i] % 2 != 0: #verifica se é impar
+            impares = i #se é impar, o valor do i(que no caso é o indice) é atribuido na variavel
+    return impares #retorna ultimo indice que foi atribuido
 
 
-lista = eval(input())
-print(f'Indice do ultimo impar {indice_ult_impar(lista)}')
+lista = eval(input()) #recebe valores, coloca na lita
+print(f'Indice do ultimo impar {indice_ult_impar(lista)}') #mostra valor
 
 
